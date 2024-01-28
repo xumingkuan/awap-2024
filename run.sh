061b8c8a3d7331d85d61baa5679dc77e1f08b63f
xumingkuan <xumingkuan0721@126.com>
2024-01-27 23:41:39 -0500

    2-bomber defense

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python ../../run_game.py -b ElaineShiFanClub.py -r ../random_bot.py -m ../../maps/biki_bott.awap24m --render
+python ../../run_game.py -b ElaineShiFanClub.py -r ElaineShiFanClub.py -m ../../maps/squig.awap24m --render
 #cd ../..
 #python run_game.py -b bots/awap-2024/ElaineShiFanClub.py -r bots/random_bot.py -m maps/three_disjoint_paths.awap24m --render
