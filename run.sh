cc0c193a43cc45b156b96abfe73098003ceee4d4
xumingkuan <xumingkuan0721@126.com>
2024-01-27 21:29:02 -0500

    greedy bomber and gunship

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python ../../run_game.py -b ElaineShiFanClub.py -r ../random_bot.py -m ../../maps/spiral.awap24m --render
+python ../../run_game.py -b ElaineShiFanClub.py -r ../random_bot.py -m ../../maps/temple.awap24m --render
