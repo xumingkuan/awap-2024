b667ffbe19af2cffe8369c7a3ed97d6be76cc662
xumingkuan <xumingkuan0721@126.com>
2024-01-28 14:36:19 -0500

    fix bomber damage estimation and add a bound to sell solar

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python ../../run_game.py -b ElaineShiFanClub.py -r solar_then_debris.py -m ../../maps/biki_bott.awap24m --render
+python ../../run_game.py -b ElaineShiFanClub.py -r ElaineShiFanClub_prev.py -m ../../maps/butterfly.awap24m --render
 #cd ../..
 #python run_game.py -b bots/awap-2024/ElaineShiFanClub.py -r bots/random_bot.py -m maps/three_disjoint_paths.awap24m --render
